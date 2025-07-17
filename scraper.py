fd113bb4a16e574fe02aa961f2500631450d1e59
Julian7793 <julian7793@gmail.com>
2025-07-17 19:13:32 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,9 @@
-# Direct USGS graph URLs by site number
 site_info = [
-    {"site_no": "03274650", "title": "Mad River"},
-    {"site_no": "03276000", "title": "Buck Creek"},
-    {"site_no": "03275000", "title": "Little Miami River"},
-    {"site_no": "03276500", "title": "Great Miami River"},
-    {"site_no": "03275990", "title": "Lagonda Ave (Daily Values)"}
+    {"site_no": "03274650", "title": "Mad River", "parm_cd": "00065"},
+    {"site_no": "03276000", "title": "Buck Creek", "parm_cd": "00065"},
+    {"site_no": "03275000", "title": "Little Miami River", "parm_cd": "00065"},
+    {"site_no": "03276500", "title": "Great Miami River", "parm_cd": "00065"},
+    {"site_no": "03275990", "title": "Lagonda Ave (Discharge)", "parm_cd": "00060"},
 ]
 
 def fetch_site_graphs():
@@ -13,11 +12,9 @@ def fetch_site_graphs():
     for site in site_info:
         site_no = site["site_no"]
         title = site["title"]
+        parm_cd = site["parm_cd"]
 
-        # Construct direct graph image URL
-        image_url = f"https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no={site_no}&period=7&parm_cd=00065"
-
-        # Page URL for reference
+        image_url = f"https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no={site_no}&period=7&parm_cd={parm_cd}"
         page_url = f"https://waterdata.usgs.gov/monitoring-location/USGS-{site_no}"
 
         site_data.append({

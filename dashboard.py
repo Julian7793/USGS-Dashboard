9d8af124678f274069989de519a30340f25d79b1
Julian7793 <julian7793@gmail.com>
2025-07-19 14:21:42 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -188,4 +188,4 @@ for i, item in enumerate(data):
                     res = requests.get(
                         f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q=47012&days=3&aqi=no&alerts=no"
                     )
-                    res.raiseres.raise_for_status()
+                    res.raise_for_status()

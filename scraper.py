77fc6955bf34d2138270f76937d840dc9aceb03e
Julian7793 <julian7793@gmail.com>
2025-08-04 16:39:45 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
 site_info = [
     {"site_no": "03274650", "title": "Whitewater River Near Economy, IN - 03274650", "parm_cd": "00065"},
-    {"site_no": "03276000", "title": "East Fork Whitewater River at Brookville, IN - 03276000", "parm_cd": "00065"},
     {"site_no": "03275000", "title": "Whitewater River Near Alpine, IN - 03275000", "parm_cd": "00065"},
     {"site_no": "03276500", "title": "Whitewater River at Brookville, IN - 03276500", "parm_cd": "00065"},
     {"site_no": "03275990", "title": "Brookville Lake at Brookville, IN - 03275990", "parm_cd": "62614"},
@@ -18,7 +21,7 @@ def fetch_site_graphs():
         page_url = f"https://waterdata.usgs.gov/monitoring-location/USGS-{site_no}"
 
         site_data.append({
-            "site_no": site_no,  # 🔹 Add this line
+            "site_no": site_no,
             "title": title,
             "image_url": image_url,
             "page_url": page_url
